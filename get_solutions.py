8a198888bd8577bb20e48671466e8503b1daab42
Rajat Asthana <rajatasthana4@gmail.com>
2018-11-04 14:35:43 +0530

    made minor changes

--- get_solutions.py
+++ get_solutions.py
@@ -5,6 +5,10 @@ from getpass import getpass
 from bs4 import BeautifulSoup
 from contextlib import contextmanager
 
+lang = {'C': '.c', 'C++': '.cpp', 'CPP14': '.cpp', 'JAVA': '.java',
+		'PYTHON3': '.py', 'PYPY': '.py', 'C++ 4.3.2': '.cpp', 'CPP': '.cpp'
+		}
+
 @contextmanager
 def login(username, password):
 	ses = requests.session()
@@ -30,15 +34,25 @@ def change_dir(destination):
 
 def get_solved_problems(soup):
 	solved_problems = set()
-	for tr in soup.find_all('tr'):
-		for td in soup.find_all('td'):
-			if td.a.text != '':
-				solved_problems.add(td.a.text)
+	tab = soup.find('table', class_='table')
+	for tr in soup.find('table', class_='table'):
+		for tr in tab.find_all('tr'):
+			for td in tab.find_all('td'):
+				if td.a.text != '':
+					solved_problems.add(td.a.text)
 	return solved_problems
 
 def get_ac_id(soup):
-	ac_id = soup.find('td').a['data-sid']
-	return ac_id
+	for tr in soup.find_all('tr'):
+		# print(tr)
+		sol_status = tr.find('td', class_='statusres text-center')
+		if sol_status != None and sol_status['status'] == '15':
+			ac_lang = tr.find('td', class_='slang').find('span').text
+			ac_id = tr.find('td', class_='statustext').text
+			print(ac_lang.strip(), ac_id.strip())
+			return (ac_lang.strip(), ac_id.strip())
+
+
 
 if __name__ == '__main__':
 	username = input("username: ")
@@ -53,10 +67,11 @@ if __name__ == '__main__':
 			url = 'https://www.spoj.com/status/' + problem + ',' + username
 			cont = session.get(url)
 			soup2 = BeautifulSoup(cont.text, 'lxml')
-			ac_id = get_ac_id(soup2)
+			print(problem + ":\n")
+			ac_lang, ac_id = get_ac_id(soup2)
 		
-			print("Downloading {}...". format(problem))
 			with change_dir("spoj_solutions"):
-				filename = problem + ".cpp"
+				filename = problem + lang[ac_lang]
+				print("Downloading {}...". format(filename))
 				with open(filename, "w") as solution:
 					solution.write(session.get('https://www.spoj.com/files/src/save/{sol_id}'.format(sol_id=ac_id)).text)
